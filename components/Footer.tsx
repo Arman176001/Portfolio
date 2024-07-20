47b8b00cb448f6a8c790a8521b4fc2abdb6ee8e4
Arman176001 <armanchaudhary176001@gmail.com>
2024-07-20 15:01:28 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -30,7 +30,7 @@ const Footer = () => {
 
         <div className="flex items-center md:gap-3 gap-6 sm:pb-5 z-20 pt-3 ">
           {socialMedia.map((info) => (
-            <a href={info.link} target="_blank">
+            <a key={info.id} href={info.link} target="_blank">
             <div
               key={info.id}
               className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
